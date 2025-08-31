38c447532cb4ef29db25184a254df9da08afdbf6
darlingson <dmakuwila@gmail.com>
2025-08-31 12:01:48 +0200

    created the backend project

---
+++ rms-be/src/main/java/app/netlify/darlingson/rms_be/RmsBeApplication.java
@@ -0,0 +1,13 @@
+package app.netlify.darlingson.rms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsBeApplication.class, args);
+	}
+
+}
---
+++ rms-be/src/test/java/app/netlify/darlingson/rms_be/RmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package app.netlify.darlingson.rms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
